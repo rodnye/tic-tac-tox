0121a1d4025867aa561d46eb32cf014fbea2628c
RodnyE <rrodnyestrada1@gmail.com>
2022-06-04 15:36:40 -0400

    splash layout added

--- js/index.js
+++ js/index.js
@@ -10,6 +10,19 @@ app.script("js/screen/s-game.js");
 function OnStart () {
   DOM = document;
   
+  /* hidden splash layout */
+  let splash = DOM.getElementById("splash-screen");
+  let animation = app.animate(function(n){
+    splash.style.filter = "opacity(" + (1 - n) + ")";
+  }, 500);
+  animation.finish(function(){
+    DOM.body.removeChild(splash);
+    splash.remove();
+    animation = null;
+    splash = null;
+  });
+  animation.start();
+  
   screen_main();
   screen_game();
 }
